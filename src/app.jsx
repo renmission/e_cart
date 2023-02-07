3fdffb829a679e41f8ebb000cc7a428b9d6d4df1
Ren Mission <missionrenjr@gmail.com>
2023-02-07 22:00:41 +0800

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import TodoListProvider from './contexts/TodoListProvider';
-import Todo from './TodoList';
 
 function App() {
   return (
-    <TodoListProvider>
-      <Todo />
-    </TodoListProvider>
+    <>App</>
   );
 }
 
